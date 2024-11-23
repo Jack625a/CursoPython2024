a4cfe4798ab88e63932b23557a4ff372086a5672
Jack625a <77822933+Jack625a@users.noreply.github.com>
2024-11-22 20:21:12 -0400

    Bucles

---
+++ bucles.py
@@ -0,0 +1,20 @@
+#Bucles (ITERAR SOBRE UNA SECUENCIA)
+#Bucles Iterativos (for)
+
+#BUCLE ITERATIVO PARA MOSTAR NUMEROS DENTRO EL INTERVALO DE 1 A 100
+for i in range(101):
+    print(i)
+
+#Iteracion sobre cadena de caracteres
+curso="PYTHON"
+for i in curso:
+    print(i)
+
+#Bucles repetitivos (While)
+#while condicion
+#Mostar los 100 digitos 
+a=1
+while a<=100:
+    print(a)
+    a+=1
+#Control de bucles (break)
